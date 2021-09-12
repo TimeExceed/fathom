4281449f7d6db1251f7af71355176f1ce1f7b179
Taoda <tyf00@aliyun.com>
2021-09-12 20:49:46 +0800

    little man

---
+++ src/fathom/tikz/little_man.py
@@ -0,0 +1,71 @@
+from .polygon import Rectangle
+from .circle import Circle
+from .arrow import Arrow, NONE
+from .opts import *
+from .. import geometry as geo
+from .. import colors
+
+class LittleMan:
+    def __init__(self, **kws):
+        self._bbox = geo.Rectangle(**kws)
+        brush_color = kws.get('brush_color', colors.INVISIBLE)
+        pen_color = kws.get('pen_color', colors.BLACK)
+        line_style = get_line_style(kws)
+        sep = kws.get('sep', 0.01)
+        vs = self._bbox.vertices()
+        ll = vs[3] + Point(sep, sep)
+        ur = vs[1] - Point(sep, sep)
+        width = ur.x - ll.x
+        height = ur.y - ll.y
+        
+        head_radius = min(width, height * 0.4) / 2
+        head_center = Point(ll.x + width / 2, ur.y - head_radius)
+        head = Circle(
+            center = head_center,
+            radius = head_radius,
+            line_style = line_style,
+            pen_color = pen_color,
+            brush_color = brush_color,
+        )
+        arm = Arrow(
+            src = Point(ll.x, ll.y + height / 2),
+            dst = Point(ur.x, ll.y + height / 2),
+            line_style = line_style,
+            pen_color = pen_color,
+            arrow_position = NONE,
+        )
+        body = Arrow(
+            src = Point((ll.x + ur.x) / 2, ur.y - head_radius * 2),
+            dst = Point((ll.x + ur.x) / 2, ll.y + height / 4),
+            line_style = line_style,
+            pen_color = pen_color,
+            arrow_position = NONE,
+        )
+        left_leg = Arrow(
+            src = Point((ll.x + ur.x) / 2, ll.y + height / 4),
+            dst = Point(ll.x + width / 4, ll.y),
+            line_style = line_style,
+            pen_color = pen_color,
+            arrow_position = NONE,
+        )
+        right_leg = Arrow(
+            src = Point((ll.x + ur.x) / 2, ll.y + height / 4),
+            dst = Point(ur.x - width / 4, ll.y),
+            line_style = line_style,
+            pen_color = pen_color,
+            arrow_position = NONE,
+        )
+        self._inner = [
+            head,
+            body,
+            arm,
+            left_leg,
+            right_leg,
+        ]
+
+    def instructions(self, insts):
+        for x in self._inner:
+            x.instructions(insts)
+
+    def get_skeleton(self):
+        return self._bbox
---
+++ test/little_man_test.py
@@ -0,0 +1,93 @@
+import testa
+from fathom import Point, ORIGIN
+import fathom.tikz as tikz
+import fathom.colors as colors
+import fathom.line_styles as line_styles
+
+@testa.is_(expect=r'''
+\documentclass[UTF8]{ctexart}
+\usepackage[a0paper]{geometry}
+\usepackage{tikz}
+\pagestyle{empty}
+
+\begin{document}
+\begin{tikzpicture}
+\draw (0.00cm,1.19cm) circle [radius=0.80cm];
+\draw (0.00cm,0.40cm) -- (0.00cm,-0.99cm);
+\draw (-1.49cm,0.00cm) -- (1.49cm,0.00cm);
+\draw (0.00cm,-0.99cm) -- (-0.74cm,-1.99cm);
+\draw (0.00cm,-0.99cm) -- (0.74cm,-1.99cm);
+\end{tikzpicture}
+\end{document}
+''')
+def draw_little_man():
+    canvas = tikz.Canvas()
+    canvas.new_little_man(center=ORIGIN, width=3, height=4)
+    return canvas.draw()
+
+@testa.is_(expect=r'''
+\documentclass[UTF8]{ctexart}
+\usepackage[a0paper]{geometry}
+\usepackage{tikz}
+\pagestyle{empty}
+
+\begin{document}
+\begin{tikzpicture}
+\draw[color=red] (0.00cm,1.19cm) circle [radius=0.80cm];
+\draw[color=red] (0.00cm,0.40cm) -- (0.00cm,-0.99cm);
+\draw[color=red] (-1.49cm,0.00cm) -- (1.49cm,0.00cm);
+\draw[color=red] (0.00cm,-0.99cm) -- (-0.74cm,-1.99cm);
+\draw[color=red] (0.00cm,-0.99cm) -- (0.74cm,-1.99cm);
+\end{tikzpicture}
+\end{document}
+''')
+def draw_red_little_man():
+    canvas = tikz.Canvas()
+    canvas.new_little_man(center=ORIGIN, width=3, height=4, pen_color=colors.RED)
+    return canvas.draw()
+
+@testa.is_(expect=r'''
+\documentclass[UTF8]{ctexart}
+\usepackage[a0paper]{geometry}
+\usepackage{tikz}
+\pagestyle{empty}
+
+\begin{document}
+\begin{tikzpicture}
+\fill[color=red] (0.00cm,1.19cm) circle [radius=0.80cm];
+\draw (0.00cm,1.19cm) circle [radius=0.80cm];
+\draw (0.00cm,0.40cm) -- (0.00cm,-0.99cm);
+\draw (-1.49cm,0.00cm) -- (1.49cm,0.00cm);
+\draw (0.00cm,-0.99cm) -- (-0.74cm,-1.99cm);
+\draw (0.00cm,-0.99cm) -- (0.74cm,-1.99cm);
+\end{tikzpicture}
+\end{document}
+''')
+def draw_little_man_with_red_head():
+    canvas = tikz.Canvas()
+    canvas.new_little_man(center=ORIGIN, width=3, height=4, brush_color=colors.RED)
+    return canvas.draw()
+
+@testa.is_(expect=r'''
+\documentclass[UTF8]{ctexart}
+\usepackage[a0paper]{geometry}
+\usepackage{tikz}
+\pagestyle{empty}
+
+\begin{document}
+\begin{tikzpicture}
+\draw[dotted] (0.00cm,1.19cm) circle [radius=0.80cm];
+\draw[dotted] (0.00cm,0.40cm) -- (0.00cm,-0.99cm);
+\draw[dotted] (-1.49cm,0.00cm) -- (1.49cm,0.00cm);
+\draw[dotted] (0.00cm,-0.99cm) -- (-0.74cm,-1.99cm);
+\draw[dotted] (0.00cm,-0.99cm) -- (0.74cm,-1.99cm);
+\end{tikzpicture}
+\end{document}
+''')
+def draw_dotted_little_man():
+    canvas = tikz.Canvas()
+    canvas.new_little_man(center=ORIGIN, width=3, height=4, line_style=line_styles.DOTTED)
+    return canvas.draw()
+
+if __name__ == '__main__':
+    testa.main()
